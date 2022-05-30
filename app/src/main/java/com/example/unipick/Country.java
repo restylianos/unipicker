75ae3b6025c3d8283b0ef2e718772dbba123a084
lizzli <50713608+restylianos@users.noreply.github.com>
2022-05-30 22:37:17 +0300

    wip clean up

--- app/src/main/java/com/example/unipick/Country.java
+++
@@ -1,8 +0,0 @@
-package com.example.unipick;
-
-public class Country {
-    University[] universities;
-    public Country(University[] universities){
-        this.universities = universities;
-    }
-}
